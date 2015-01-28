34521fe2c0d5bc855ee8d43f76d2b5073c28f8d9
Nathan Bateman <nathan.bateman.jr@gmail.com>
2015-01-28 15:14:07 +0700

    moved the Math.sin calculation to a var outside the for loop

--- views/js/main.js
+++ views/js/main.js
@@ -503,9 +503,10 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var sTop = Math.sin((document.body.scrollTop / 1250);
   var numberOfItems = items.length;
   for (var i = 0; i < numberOfItems; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = sTop + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
