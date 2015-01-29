dd0d28327c87c369f6f5a3b646216742aeb15c81
Nathan Bateman <nathan.bateman.jr@gmail.com>
2015-01-29 17:57:40 +0700

    restored pizza modification function to original logic

--- views/js/main.js
+++ views/js/main.js
@@ -450,8 +450,7 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    var getRandoms = document.querySelectorAll(".randomPizzaContainer").length;
-    for (var i = 0; i < getRandoms; i++) {
+    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
